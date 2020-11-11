7c5d2b2188405ca0ffce2b969980aaa393ee6ab9
Ilia Colombini <ilia.colombini@gmail.com>
2020-11-11 15:21:59 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [:new, :create]
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [:new, :create, :index]
   end
-  resources :reviews, only: :destroy
-
-  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
 end
